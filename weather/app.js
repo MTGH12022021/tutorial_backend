97c50cbcbbfc7c670c3c24c31244d9ab46744938
MTGH12022021 <loicho0167277@gmail.com>
2022-06-24 12:12:00 +0700

    update weather api

--- weather/app.js
+++ weather/app.js
@@ -12,15 +12,21 @@ app.use(bodyParser.json());
 var weatherData = "";
 
 app.get('/', (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=cde2559701270c589912f9c21f548f07#?unit=metric";
+    const query = "HaNoi";
+    const apiKey = "cde2559701270c589912f9c21f548f07";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+    const units = "metric";
     https.get(url, (respond) => {
         //console.log(res);
         respond.on('data', (data) => {
             weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageIconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<h1 style='color: brown'> Weather </h1>");
-            res.write("<h2> The temperature in Lodon is " + temp + "</h2>");
+            res.write("<h2> The temperature in "+ query +" is " + temp + "</h2>");
+            res.write("<image src= " + imageIconUrl+ "></image>")
             res.send();
         });
     });
