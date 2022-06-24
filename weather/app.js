2283831795933ddceffde56ed5851268dce02862
MTGH12022021 <loicho0167277@gmail.com>
2022-06-24 13:03:21 +0700

    update background weather api

--- weather/app.js
+++ weather/app.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
-const { join } = require('path');
+const path = require('path');
+const { dirname } = require('path');
 
 
 const POST = 3000;
 const app = express();
 
-app.use(bodyParser.json());
+app.use('/css', express.static(path.join(__dirname + "/html/css")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var weatherData = "";
 
 app.get('/', (req, res) => {
-    const query = "HaNoi";
+    res.sendFile(path.join(__dirname, "/html", "index.html"));
+});
+
+app.post('/weather', (req, res) => {
+    console.log(req.body.cityName);
+
+    const query = req.body.cityName;
     const apiKey = "cde2559701270c589912f9c21f548f07";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
     const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
     https.get(url, (respond) => {
-        //console.log(res);
+
         respond.on('data', (data) => {
             weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
@@ -25,17 +33,21 @@ app.get('/', (req, res) => {
             const icon = weatherData.weather[0].icon;
             const imageIconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<h1 style='color: brown'> Weather </h1>");
-            res.write("<h2> The temperature in "+ query +" is " + temp + "</h2>");
-            res.write("<image src= " + imageIconUrl+ "></image>")
+            res.write("<h2> The temperature in " + query + " is " + temp + "</h2>");
+            res.write("<image src= " + imageIconUrl + "></image>")
             res.send();
         });
     });
-    //res.send("suscessfully")
 });
 
-app.get('/data', (req, res) => {
-    res.send("<h1>nguyen tien loi" + weatherData.weather + "</h1>");
-})
+
+
+
+
+
+// app.get('/data', (req, res) => {
+//     res.send("<h1>nguyen tien loi" + weatherData.weather + "</h1>");
+// })
 
 app.listen(POST);
 
