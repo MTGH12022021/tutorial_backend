50c34560f7d5b281c04cba8ebd33d7b8cca41e27
MTGH12022021 <loicho0167277@gmail.com>
2022-06-24 11:15:19 +0700

    login basic update

--- login/app.js
+++ login/app.js
@@ -5,6 +5,13 @@ const { request } = require('http');
 const bodyParser = require('body-parser'); //https://www.npmjs.com/package/body-parser#bodyparserurlencodedoptions
 
 const path = require('path');
+
+//module xu ly coi la cau truc file json co dung nhu la ta mong muon khong
+//https://viblo.asia/p/verify-json-request-nodejs-voi-joi-V3m5WLpgKO7
+const Joi = require('joi');
+
+
+
 const app = express();
 
 
@@ -15,6 +22,8 @@ app.use('/css', express.static(__dirname + '/html/css'));
 
 // su dung phan mem trung gian theo kieu mo trong va phan tich tai khoan mat khau duoi dang chuoi nen la false
 app.use(bodyParser.urlencoded({ extended: true }));
+// su dung phan mem trung gian de doc file json
+app.use(bodyParser.json());
 
 app.get('/login', (request, respond) => {
     respond.sendFile(path.join(__dirname, './html', 'index.html'));
@@ -22,6 +31,10 @@ app.get('/login', (request, respond) => {
 
 // app.get('/logout', (request, respond) => {
 //     respond.sendFile(path.join(__dirname, './w3_band', 'index.html'));
+// });
+
+// app.get('/logout', (request, respond) => {
+//     respond.send("oke chien");
 // });
 
 // cho phep nguoi dung dua thong tin len server
@@ -32,8 +45,24 @@ app.post('/logout', (request, respond) => {
 
     console.log(request.body);
 
+    // xac thuc lai coi mail pass co dung cau truc khong
+    var schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(20).required()
+    });
+
+    const { error } = schema.validate(request.body);
+    if (error) {
+        respond.send(error.details[0].message);
+    }
+    else {
+        respond.send("sucessfully")
+    }
+
+
     //database word here
-    respond.send('sucessfully posted data');
+    //respond.send("oke thanh cong");
+
 });
 
-app.listen(3000); 
+app.listen(3000);
